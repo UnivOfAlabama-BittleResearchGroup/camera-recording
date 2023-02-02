1e695e787121ef5ba5bcf01c97802d8713685ed8
Nigel Liu <hliu52@crimson.ua.edu>
2023-02-02 14:58:23 -0600

    working rec get req

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
-import xmlschema
 import requests
+import dotenv
+import os
+import xmltodict, json
 
-ip = "static ip of camera"
-disk_id = "SD_DISK" # check with get request
-recording_request = f"//{ip}/axis-cgi/record/list.cgi?recordingid=all"
-recording_list = requests.get(recording_request)
+dotenv.load_dotenv()
+
+ip = os.environ.get("IP")
+disk_id = "SD_DISK"
+
+recording_request = f"http://{ip}/axis-cgi/record/list.cgi?recordingid=all"
+recording_list = requests.get(recording_request).content.decode()
 print(recording_list)
+rec_dict = xmltodict.parse(recording_list)
 
-export_request = f"//{ip}/axis-cgi/record/export/exportrecording.cgi?schemaversion=1&recordingid={recording_list}&diskid={disk_id}&exportformat=matroska"
-export_list = requests.get(export_request)
-print(export_list)
 
-delete_request = f"//{ip}/axis-cgi/record/remove.cgi?recordingid={recording_list}"
+#export_request = f"//{ip}/axis-cgi/record/export/exportrecording.cgi?schemaversion=1&recordingid={recording_list}&diskid={disk_id}&exportformat=matroska"
+#export_list = requests.get(export_request)
+#print(export_list)
+
+#delete_request = f"//{ip}/axis-cgi/record/remove.cgi?recordingid={recording_list}"
 
 
 # for camera in cameras:
