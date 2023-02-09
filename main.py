88004a3b47e27e63b3a7700dc4b2e0e0e5a8f43a
Arya Karnik <abkarnik@crimson.ua.edu>
2023-02-08 20:46:00 -0600

    working API download request for n-number of recordings on camera SD card

--- main.py
+++ main.py
@@ -1,22 +1,44 @@
 import requests
 import dotenv
 import os
-import xmltodict, json
+import xmltodict
+import pprint
+import json
 
 dotenv.load_dotenv()
 
 ip = os.environ.get("IP")
+ex_password = os.environ.get("EXPORT_PASS") # password for encrypted zip files
 disk_id = "SD_DISK"
 
 recording_request = f"http://{ip}/axis-cgi/record/list.cgi?recordingid=all"
-recording_list = requests.get(recording_request).content.decode()
-print(recording_list)
-rec_dict = xmltodict.parse(recording_list)
+recording_xml = requests.get(recording_request).content.decode()
 
+rec_dict = xmltodict.parse(recording_xml)
+recording_list = rec_dict['root']['recordings']['recording']
+filtered_recording_id = [recording['@recordingid'] for recording in recording_list]
+# print(filtered_recording_id)
+# pprint.pprint(rec_dict)
+for id in filtered_recording_id:
+    # export_format = (f"http://{ip}/axis-cgi/record/list.cgi?recordingid=all")
+    # format_xml = requests.get(export_format).content.decode()
+    # pprint.pprint(format_xml)
+    # print(id)
+    # export_capabilities = f"http://{ip}/axis-cgi/record/export/properties.cgi?schemaversion=1&recordingid={id}&diskid={disk_id}"
+    # capabilities_list = requests.get(export_capabilities).content.decode()
+    # pprint.pprint(capabilities_list)
 
-#export_request = f"//{ip}/axis-cgi/record/export/exportrecording.cgi?schemaversion=1&recordingid={recording_list}&diskid={disk_id}&exportformat=matroska"
-#export_list = requests.get(export_request)
-#print(export_list)
+    export_request_mkv = (f"http://{ip}/axis-cgi/record/export/exportrecording.cgi?schemaversion=1&recordingid={id}&diskid={disk_id}&exportformat=matroska")
+    request_xml = requests.get(export_request_mkv).content.decode()
+    pprint.pprint(request_xml)
+
+    # export_request_zip = (f"http://{ip}/axis-cgi/record/export/exportrecording.cgi?schemaversion=1"
+    # f"&recordingid={id}&diskid={disk_id}&exportformat=matroska&starttime=2023-02-03T17:45:09Z&stoptime=2023-02-03T17:45:15")
+    # request_xml = requests.get(export_request_zip).content.decode()
+    # print(export_request_zip)
+    # pprint.pprint(request_xml)
+
+    
 
 #delete_request = f"//{ip}/axis-cgi/record/remove.cgi?recordingid={recording_list}"
 
